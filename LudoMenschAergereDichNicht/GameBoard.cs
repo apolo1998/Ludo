6d4ebc74b5a3f8c3a2ddc2233c8fb0391c5e5c62
apolo1998 <paoloortlinghaus@gmail.com>
2025-10-01 17:07:56 +0200

    Creation of base classes

---
+++ LudoMenschAergereDichNicht/GameBoard.cs
@@ -0,0 +1,6 @@
+﻿namespace LudoMenschAergereDichNicht;
+
+public class GameBoard
+{
+    
+}
---
+++ LudoMenschAergereDichNicht/GameField.cs
@@ -0,0 +1,3 @@
+﻿namespace LudoMenschAergereDichNicht;
+
+public record GameField(int index, bool start, Player player, bool isOccupied, bool isGoal );
